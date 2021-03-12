253dd0374ce950251a5a7511b2bd6c1d942323f4
jorisAllgeyer <joevictorallgeyer@gmail.com>
2021-03-12 10:51:20 +0100

    begin rotation handling

--- app/src/main/java/fr/doranco/myquizz/view/IConst.java
+++ app/src/main/java/fr/doranco/myquizz/view/IConst.java
@@ -5,4 +5,7 @@ public interface IConst {
     String PREF_KEY_USER_NAME = "PREF_KEY_USER_NAME";
     String PREF_KEY_USER_SCORE = "PREF_KEY_USER_SCORE";
     String EXTRA_SCORE_PARAM = "EXTRA_SCORE_PARAM";
+
+    String STATE_SCORE = "STATE_SCORE";
+    String STATE_QUESTION = "STATE_QUESTION";
 }
--- app/src/main/java/fr/doranco/myquizz/view/QuizActivity.java
+++ app/src/main/java/fr/doranco/myquizz/view/QuizActivity.java
@@ -1,8 +1,14 @@
 package fr.doranco.myquizz.view;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -32,6 +38,14 @@ public class QuizActivity extends AppCompatActivity implements IConst, View.OnCl
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizz);
 
+        if (savedInstanceState != null) {
+            score = savedInstanceState.getInt(STATE_SCORE);
+            questionAsked = savedInstanceState.getInt(STATE_QUESTION);
+        } else {
+            score = 0;
+            questionAsked = 4;
+        }
+
         tvQuestion = findViewById(R.id.tvQuestion);
         btnAnswer1 = findViewById(R.id.btnA1);
         btnAnswer2 = findViewById(R.id.btnA2);
@@ -39,9 +53,7 @@ public class QuizActivity extends AppCompatActivity implements IConst, View.OnCl
         btnAnswer4 = findViewById(R.id.btnA4);
 
         questionBank = generateQuestions();
-
-        score = 0;
-        questionAsked = maxQuestions = 4;
+        maxQuestions = 4;
 
         // Listeners
         btnAnswer1.setTag(0);
@@ -67,6 +79,7 @@ public class QuizActivity extends AppCompatActivity implements IConst, View.OnCl
 
     @Override
     public void onClick(View view) {
+        // View is the page, you can get only one tag per view.
         int responseIndex = (int) view.getTag();
 
         if (responseIndex == currentQuestion.getCorrectAnswerIndex()) {
@@ -76,17 +89,43 @@ public class QuizActivity extends AppCompatActivity implements IConst, View.OnCl
             Toast.makeText(QuizActivity.this, "Mauvaise réponse !", Toast.LENGTH_SHORT).show();
         }
 
-        if (--questionAsked == 0) {
-            endQuiz();
-        } else {
-            currentQuestion = questionBank.getNextQuestion();
-            displayCurrentQuestion(currentQuestion);
-        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (--questionAsked == 0) {
+                    endQuiz();
+                } else {
+                    currentQuestion = questionBank.getNextQuestion();
+                    displayCurrentQuestion(currentQuestion);
+                }
 
+            }
+        }, 2000);
     }
 
     private void endQuiz() {
         // TODO
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Quiz terminé")
+                .setMessage("Votre score est de: " + score + "/" + maxQuestions)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent();
+                        intent.putExtra(EXTRA_SCORE_PARAM, score);
+                        setResult(RESULT_OK, intent);
+                        finish();
+                    }
+                })
+                .create()
+                .show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putInt(STATE_SCORE, score);
+        outState.putInt(STATE_QUESTION, questionAsked);
+        super.onSaveInstanceState(outState);
     }
 
     private QuestionBank generateQuestions() {
@@ -103,7 +142,7 @@ public class QuizActivity extends AppCompatActivity implements IConst, View.OnCl
                 Arrays.asList("Vicent Auriol", "René Coty", "Albert Lebrun", "Paul Doumer"),0);
 
         Question question5 = new Question("Quel est la plus petite République du Monde ?",
-                Arrays.asList("Nauru", "Monaco", "Les Palaos", "Les Tuvalu"),1);
+                Arrays.asList("Nauru", "Monaco", "Les Palaos", "Les Tuvalu"),0);
 
         Question question6 = new Question("Quelle est la langue la moins parlée au monde ?",
                 Arrays.asList("L'artchi", "Le Silbo", "Rotokas", "le Piraha"),0);
